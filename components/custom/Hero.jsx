a907f2c8a1952896b0f34b8044bf5322497ccfa5
dennis-mmachoene <dennismmachoene@gmail.com>
2025-03-06 13:49:42 +0200

    Created Workspace

--- components/custom/Hero.jsx
+++ components/custom/Hero.jsx
@@ -1,36 +1,51 @@
 "use client";
-import {MessagesContext} from "@/context/MessagesContext";
-import {UserDetailsContext} from "@/context/UserDetailsContext";
+import { MessagesContext } from "@/context/MessagesContext";
+import { UserDetailsContext } from "@/context/UserDetailsContext";
 import Colors from "@/data/Colors";
 import Lookup from "@/data/Lookup";
 import { ArrowRight, Link } from "lucide-react";
 import React, { useContext, useState } from "react";
 import LoginDialog from "./LoginDialog";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useRouter } from "next/navigation";
 
 const Hero = () => {
   const [userInput, setUserInput] = useState();
+  const { messages, setMessages } = useContext(MessagesContext);
+  const { userDetails, setUserDetails } = useContext(UserDetailsContext);
+  const [openDialog, setOpenDialog] = useState(false);
+  const CreateWorkSpace = useMutation(api.workspace.CreateWorkSpace);
+  const router = useRouter();
 
-  const { messages, setMessages } = useContext(MessagesContext)
-  const {userDetails, setUserDetails} = useContext(UserDetailsContext)
-  const [openDialog, setOpenDialog] = useState(false)
-
-  const onGenerate = (input) => {
-    if(!userDetails?.name) {
-      setOpenDialog(true)
-      return ;
+  const onGenerate = async (input) => {
+    if (!userDetails?.name) {
+      setOpenDialog(true);
+      return;
     }
-    setMessages({
-      role: 'user',
-      content: input
-    })
-  }
+    const msg = {
+      role: "user",
+      content: input,
+    };
+    setMessages(msg);
+
+    const workspaceId = await CreateWorkSpace({
+      user: userDetails._id,
+      messages: [msg],
+    });
+    console.log(workspaceId)
+    router.push('/workspace/'+workspaceId)
+  };
   return (
     <div className="flex flex-col items-center mt-36 xl:mt-52 gap-2">
       <h2 className="font-bold text-4xl">{Lookup.HERO_HEADING}</h2>
       <p className="text-gray-400 font-medium">{Lookup.HERO_DESC}</p>
-      <div className="p-5 border rounded-xl max-w-xl w-full mt-3" style={{
-        backgroundColor: Colors.BACKGROUND
-      }}>
+      <div
+        className="p-5 border rounded-xl max-w-xl w-full mt-3"
+        style={{
+          backgroundColor: Colors.BACKGROUND,
+        }}
+      >
         <div className="flex gap-2">
           <textarea
             onChange={(e) => setUserInput(e.target.value)}
@@ -40,7 +55,8 @@ const Hero = () => {
           {userInput && (
             <ArrowRight
               onClick={() => onGenerate(userInput)}
-              className="bg-blue-500 p-2 h-10 w-8 rounded-md cursor-pointer" />
+              className="bg-blue-500 p-2 h-10 w-8 rounded-md cursor-pointer"
+            />
           )}
         </div>
         <div>
@@ -52,12 +68,15 @@ const Hero = () => {
           return (
             <h2
               onClick={() => onGenerate(suggestion)}
-              className=" text-sm text-gray-400 hover:text-white cursor-pointer p-1 px-2 border rounded-full" key={index}>{suggestion}</h2>
-          )
+              className=" text-sm text-gray-400 hover:text-white cursor-pointer p-1 px-2 border rounded-full"
+              key={index}
+            >
+              {suggestion}
+            </h2>
+          );
         })}
       </div>
       <LoginDialog openDialog={openDialog} setOpenDialog={setOpenDialog} />
-
     </div>
   );
 };
